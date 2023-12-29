0c6d10d04566b243e1bafd51cc14c93c1d1aab93
Lucas Batista <lucasjsbatista@gmail.com>
2023-12-28 21:23:12 -0400

    Changes in home page and contact page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0eb43cb9413040b"; }
+/******/ 		__webpack_require__.h = function() { return "212ddb60df1b2dcb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -22,12 +22,12 @@ export default function Home() {
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                                        <li class="nav-item"><a class="nav-link" href="">Home</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                                         <li class="nav-item"><a class="nav-link" href="">Quem somos</a></li>
                                         <li class="nav-item"><a class="nav-link" href="">Serviços</a></li>
                                         <li class="nav-item"><a class="nav-link" href="">Provas</a></li>
                                         <li class="nav-item"><a class="nav-link" href="">Resultados</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="">Contato</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="/contato">Contato</a></li>
                                     </ul>
                                 </div>
                             </div>
