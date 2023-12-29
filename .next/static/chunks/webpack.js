64caf0fe84db0dd024afc535e32362edcdc37db8
Lucas Batista <lucasjsbatista@gmail.com>
2023-12-28 21:09:03 -0400

    New template applied

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5392d4345e649f07"; }
+/******/ 		__webpack_require__.h = function() { return "b0eb43cb9413040b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,62 +1,65 @@
 import Head from 'next/head'
-import { Container, Row, Card, Button } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <Container className="md-container">
       <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
         <title>Home - DIGITIME</title>
+        <link href="style/index.css" rel="stylesheet" />
       </Head>
       <Container>
-        <div class="col-lg-8 mx-auto p-4 py-md-5" />
-            <header class="d-flex align-items-center pb-3 md-5">
-                <div>
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <a class="navbar-brand" href="#">
-                            <img src="/digitime_logo.png" width="300" alt="" />
-                        </a>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="index.html">Quem somos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Serviços</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Provas</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="resultados.html">Resultados</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contato.html">Contato</a>
-                            </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search" />
-                            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Pesquisar</button>
-                        </form>
+        <div>
+            <body class="d-flex flex-column h-100">
+                <main class="flex-shrink-0">
+                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <div class="container px-5">
+                                <img class="img-fluid rounded-3" width="250px" src="/digitime_logo3.png" alt="..." />
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                        <li class="nav-item"><a class="nav-link" href="">Home</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="">Quem somos</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="">Serviços</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="">Provas</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="">Resultados</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="">Contato</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                        <header class="bg-dark py-5">
+                            <div class="container px-5">
+                                <div class="row gx-5 align-items-center justify-content-center">
+                                    <div class="col-lg-8 col-xl-7 col-xxl-6">
+                                        <div class="my-5 text-center text-xl-start">
+                                            <h1 class="display-5 fw-bolder text-white mb-2">Bem vindo ao nosso mundo</h1>
+                                            <p class="lead fw-normal text-white-50 mb-4">A DIGITIME INOVA, empresa genuinamente amazonense, atua no ramo de cronometragem eletrônica de eventos esportivos, com a maior tecnologia existente, além de uma equipe especializada e dedicada a produzir resultados com rapidez e confiabilidade.</p>
+                                            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                                                <a class="btn btn-primary btn-lg px-4 me-sm-3" href="">Resultados de Provas</a>
+                                                {/* <a class="btn btn-outline-light btn-lg px-4" href="">Entrar em contato</a> */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="/empresa_digitime.jpg" alt="..." /></div>
+                                </div>
+                            </div>
+                        </header>
+                    </main>
+                <footer class="bg-dark py-4 mt-auto">
+                    <div class="container px-5">
+                        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                            <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Digitime 2023</div></div>
                         </div>
-                    </nav>
-                </div>
-            </header>
-            <main>
-                <div class="container-fluid mb-5">
-                <h1 class="text-body-emphasis">Bem vindo ao nosso mundo.</h1>
-                </div>
-                <div class="container-fluid mb-5">
-                <p>
-                    A DIGITIME INOVA, empresa genuinamente amazonense, atua no ramo de cronometragem eletrônica de eventos 
-                    esportivos, com a maior tecnologia existente, além de uma equipe especializada e dedicada a produzir resultados com rapidez e confiabilidade.
-                </p>
-                </div>
-            </main>
-      </Container>
-      <footer class="pt-5 my-5 text-body-secondary border-top">© Digitime 2023 </footer>
+                    </div>
+                </footer>
+            </body>
+        </div>
+        </Container> 
     </Container>
   )
 }
