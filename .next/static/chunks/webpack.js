af7f383ea208869f46b76574e5c3f0ba8e603ed0
Lucas Batista <lucasjsbatista@gmail.com>
2024-01-24 12:45:52 -0400

    Installing dotenv and updating gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f8828c2cd67207b"; }
+/******/ 		__webpack_require__.h = function() { return "518ca5b057cae226"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
